1b7dc79d50f7d133792a7cc85e9714ff8b8d136b
Nwaiwuchi10 <djnchrys@gmail.com>
2024-09-13 04:13:38 -0700

    question stats

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -153,14 +153,14 @@ export class QuestionsController {
     const userId = req.userId; // Assuming the user ID is stored in the request object after authentication
     return this.questionsService.changeQuestionStatus(questionId, adminId);
   }
-  @Get(':id/viewQuestion')
+  @Get(':id/stats')
   @UseGuards(UserAuthGuard)
   async viewQuestion(@Param('id') id: string, @Req() req) {
     const userId = req.userId; // Assuming user authentication and userId extraction are set up
     return await this.questionsService.viewQuestion(id, userId);
   }
 
-  @Get(':id/totalViews')
+  @Get(':id/totalStats')
   async getQuestionViews(@Param('id') id: string) {
     return await this.questionsService.getQuestionViews(id);
   }
