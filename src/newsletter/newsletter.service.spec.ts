6577085b1e309ae1bf10fdebf1669d08f8deef76
Mac <mac@Macs-MacBook-Pro.local>
2024-10-09 11:18:16 +0100

    update

--- src/newsletter/newsletter.service.spec.ts
+++ src/newsletter/newsletter.service.spec.ts
@@ -12,7 +12,7 @@ describe('NewsletterService', () => {
     service = module.get<NewsletterService>(NewsletterService);
   });
 
-  it('should be defined', () => {
+  it('should be defined!', () => {
     expect(service).toBeDefined();
   });
 });
